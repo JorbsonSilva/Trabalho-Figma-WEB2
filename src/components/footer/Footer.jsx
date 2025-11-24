187bab067e1d8235e4660db4dda27958e6e1e2d3
Jorbson <jorbsonsilva93@gmail.com>
2025-11-24 16:23:32 -0300

    rodape finalizado.

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,4 +1,5 @@
 import styles from "./Footer.module.css";
+import Image from "next/image";
 
 export default function Footer() {
     return (
@@ -7,34 +8,76 @@ export default function Footer() {
             <div className={styles.footerContent}>                
                
                 <div className="footer-col-info">
-                    {/* Aqui vai o Logo e o texto descritivo */}
-                    {/* Ícones de redes sociais */}
+                    <div className={styles.logo}>
+                         <Image src="/images/logo-icon.png" alt="Caruaru Têxtil Hub" width={30} height={30} />
+                    <div>
+                        <h2>Caruaru Têxtil Hub</h2>
+                        <p>A Sulanca Online</p>
+                    </div>
+
+                    
+                </div>  
+                <div className="descricao">O maior marketplace têxtil do Nordeste,conectando fabricantes de Caruaru com 
+                    compradores de todo o Brasil. Tradição e inovação em um só lugar.</div>      
+
+                <div className={styles.redeSocial}>
+                    <Image src="/images/facebook.png" alt="facebook" width={20} height={20} />
+                    <Image src="/images/instagram.png" alt="instagram" width={20} height={20} />
+                    <Image src="/images/twitter-alt-square.png" alt="twitter" width={20} height={20} />
+                    <Image src="/images/youtube.png" alt="youtube" width={20} height={20} />
+                                   
                 </div>
+                    <br />          
+                <p>&copy; 2025 Caruaru Têxtil Hub.</p>
+            </div>
 
                 
-                <div className="footer-col-links">
+                <div className="footer-col-links">                    
                     <h3>Links Rápidos</h3>
-                    {/* Lista de links */}
+                    <br />
+                    <div className={styles.footerContato}>
+                        <ul>
+                            <li>Todos os Produtos</li>
+                            <li>Nossos Vendedores</li>
+                            <li>Sobre Nós</li>
+                            <li>Como Vender</li>
+                            <li>Política de Privacidade</li>
+                        </ul>
+                    </div>
+                    
                 </div>
 
                 
                 <div className="footer-col-categorias">
                     <h3>Categorias</h3>
-                    {/* Lista de categorias */}
+                    <br />
+                    <div className={styles.footerContato}>
+                        <ul>
+                            <li>Jeans e Denim</li>
+                            <li>Malhas e Camisetas</li>
+                            <li>Moda Infatil</li>
+                            <li>Moda Feminina</li>
+                            <li>Moda Masculina</li>
+                        </ul>
+                    </div>
+                    
                 </div>
 
                 
                 <div className="footer-col-contato">
                     <h3>Contato</h3>
-                    {/* Newsletter e informações de telefone */}
+                    <br />
+                    <div className={styles.footerContato}>
+                        <ul>
+                            <li><span><Image src="/images/location-pin.png" alt="Localização" width={14} height={14} /> Caruaru, PE - Polo Têxtil do Nordeste</span></li>
+                            <li><span><Image src="/images/circle-phone.png" alt="telefone" width={14} height={14} /> (81) 3000-0000</span></li>
+                            <li><span><Image src="/images/envelope.png" alt="e-mail" width={14} height={14} /> contato@caruarutextil.com.br</span></li>
+                        </ul>
+                    </div>
                 </div>
 
-            </div>
+            </div>                       
             
-            
-            <div className="footer-copy">
-                <p>&copy; 2025 Caruaru Têxtil Hub.</p>
-            </div>
         </footer>
     );
 }
