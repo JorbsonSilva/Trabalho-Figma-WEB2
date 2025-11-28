5a1cd860fd1013d1603c1d0ccc470472af9ec202
Thiago Rodrigues <thiagorodrigues2425@gmail.com>
2025-11-28 13:37:03 -0300

    correção de imports nas páginas 'login e registro'

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,17 +1,24 @@
-"use client";
-import LoginScreen from '@/components/login/LoginScreen';
-import RegisterScreen from '@/components/registro/RegisterScreen';
+'use client';
 
 import { useState } from 'react';
+import LoginScreen from '@components/login/LoginScreen';
+import RegisterScreen from '@components/registro/RegisterScreen';
 
-function App() {
+function LoginPage() {
   const [currentScreen, setCurrentScreen] = useState('login'); 
+  
   const handleSwitchScreen = (screenName) => {
     setCurrentScreen(screenName);
   };
 
   return (
-    <div className="App" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '100vh', backgroundColor: '#1E1E3F' }}>
+    <div className="App" style={{ 
+      display: 'flex', 
+      justifyContent: 'center', 
+      alignItems: 'center', 
+      minHeight: '100vh', 
+      backgroundColor: '#1E1E3F' 
+    }}>
       {currentScreen === 'login' ? (
         <LoginScreen onSwitchToRegister={() => handleSwitchScreen('register')} />
       ) : (
@@ -20,4 +27,5 @@ function App() {
     </div>
   );
 }
-export default App;
+
+export default LoginPage;
